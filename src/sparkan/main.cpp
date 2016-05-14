6f8987e91abb88a1e94c66662a00eeb47cca6333
Nemanja Hirsl <nemhirsl@gmail.com>
2016-05-15 00:48:58 +0200

    Adding App details after registration.

--- src/sparkan/main.cpp
+++ src/sparkan/main.cpp
@@ -5,6 +5,12 @@
 #include <KLocalizedString>
 #include "mainwindow.h"
 
+static std::string clientSecret = "5a1b187745e79a6cb2fe6b005a3b3be16b633411374eacb84a38c3c4068fbf50";
+static std::string clientId = "Cf058c9b9c30412326fe40868e333796bfeae17fb58ef709de8a7e8c73850ceca";
+static std::string oauthAuthorizationUrl = "https://api.ciscospark.com/v1/authorize?client_id=Cf058c9b9c30412326fe40868e333796bfeae17fb58ef709de8a7e8c73850ceca"
+                                           "&response_type=code&redirect_uri=http%3A%2F%2Flocalhost&scope=spark%3Amessages_write%20spark%3Arooms_read%20spark%3"
+                                           "Amemberships_read%20spark%3Amessages_read%20spark%3Arooms_write%20spark%3Apeople_read%20spark%3Amemberships_write&state=set_state_here";
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
