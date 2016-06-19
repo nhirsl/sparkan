3c7cf2a0c6a1ae6308db0e0d1b4331f5c0740f6c
Nebojsa Karan <nebojsakaran1011@gmail.com>
2016-06-19 21:02:44 +0200

    Auth 4

--- src/sparkan/webauth.cpp
+++ src/sparkan/webauth.cpp
@@ -13,6 +13,10 @@ static std::string oauthAuthorizationUrl = "https://api.ciscospark.com/v1/author
                                            "&response_type=code&redirect_uri=http%3A%2F%2Flocalhost&scope=spark%3Amessages_write%20spark%3Arooms_read%20spark%3"
                                            "Amemberships_read%20spark%3Amessages_read%20spark%3Arooms_write%20spark%3Apeople_read%20spark%3Amemberships_write&state=set_state_here";
 
+
+
+
+
 WebAuth::WebAuth() {}
 
 void WebAuth::GetAuth()
@@ -26,11 +30,10 @@ void WebAuth::GetAuth()
     widget->setWindowState(Qt::WindowMaximized);
     widget->show();
 
-    connect(view, SIGNAL(urlChanged(QUrl)),     this, SLOT(on_ulrChanged(QUrl)));
-    //connect(view, SIGNAL(loadStarted()),        this, SLOT(on_loadStarted()));
-    //connect(view, SIGNAL(loadFinished(bool)),   this, SLOT(on_loadFinished(bool)));
-    //connect(view, SIGNAL(loadProgress(int)),    this, SLOT(on_loadProgress(int)));
-    //connect(view, SIGNAL(linkClicked(QUrl)),    this, SLOT(on_linkClicked(QUrl)));
+    connect(view->page()->mainFrame(), SIGNAL(urlChanged(QUrl)),     this, SLOT(on_ulrChanged(QUrl)));
+    connect(view, SIGNAL(loadStarted()),        this, SLOT(on_loadStarted()));
+    connect(view, SIGNAL(loadFinished(bool)),   this, SLOT(on_loadFinished(bool)));
+    connect(view->page()->networkAccessManager(), SIGNAL(finished(QNetworkReply*)), this, SLOT(nm_Finished(QNetworkReply*)));
 
     QUrl url (oauthAuthorizationUrl.c_str());
 
@@ -39,48 +42,44 @@ void WebAuth::GetAuth()
 }
 
 
-void WebAuth::on_ulrChanged(QUrl url)
-{
-    //qWarning() << "Url Changed: " << url << "\n";
-    QRegularExpression rx("[&?]code=(.+?)[&^$]");
-    QRegularExpressionMatch match = rx.match(url.toString());
-    if (match.hasMatch())
-    {
-        qWarning() << "Url Changed: " << url << "\n";
-
-        qWarning() << "Captured: " << match.captured(1);
-        QNetworkRequest *request = new QNetworkRequest(QUrl("https://api.ciscospark.com/v1/access_token"));
-        request->setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
-        QNetworkAccessManager *nm = new QNetworkAccessManager();
-        QString postData = "grant_type=authorization_code&"
-                           "client_id=Cf058c9b9c30412326fe40868e333796bfeae17fb58ef709de8a7e8c73850ceca&"
-                           "client_secret=5a1b187745e79a6cb2fe6b005a3b3be16b633411374eacb84a38c3c4068fbf50&"
-                           "code=" + match.captured(1) + "&redirect_uri=http%3A%2F%2Flocalhost";
-
-        qWarning() << " Auth url: " << postData;
-        connect(nm, SIGNAL(finished(QNetworkReply*)), this, SLOT(on_loadFinished(QNetworkReply*)));
-        nm->post(*request, postData.toUtf8());
-    }
-}
-
-void WebAuth::on_loadStarted()
-{
-    qWarning() << "loadStarted: ";
-}
-
-void WebAuth::on_loadFinished(QNetworkReply* reply)
+void WebAuth::on_loadFinished(bool ok)
 {
     //qWarning() << "loadFinished: " << reply->readAll();
-
-    qWarning() << "Http \n" << view->page()->mainFrame()->toHtml() << "\n";
+    //qWarning() << "loadFinished: ";
+    //qWarning() << "Http ***********************************************************************\n"
+    //           << view->page()->mainFrame()->toHtml()
+    //           << "******************************************************************************\n";
 }
 
-void WebAuth::on_loadProgress(int progress)
-{
-    qWarning() << "loadProgress: ";
-}
 
-void WebAuth::on_linkClicked(QUrl url)
-{
-    qWarning() << "linkClicked: " << url.toString();
+void WebAuth::nm_Finished(QNetworkReply *reply){
+    if (reply->error() == QNetworkReply::NoError) {
+        int statusCode = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+        switch (statusCode) {
+            case 301:
+            case 302:
+            case 307:
+            QUrl url = reply->attribute(QNetworkRequest::RedirectionTargetAttribute).toUrl();
+            QRegularExpression rx("localhost.*[&?]code=(.+?)[&^$]");
+            QRegularExpressionMatch match = rx.match(url.toString());
+            if (match.hasMatch())
+            {
+                qWarning() << "Captured: " << match.captured(1);
+                QNetworkRequest *request = new QNetworkRequest(QUrl("https://api.ciscospark.com/v1/access_token"));
+                request->setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+                request->setRawHeader(QByteArray("Accept"), QByteArray("application/json"));
+                QNetworkAccessManager *nm = new QNetworkAccessManager();
+                QString postData = "grant_type=authorization_code&"
+                                   "client_id=Cf058c9b9c30412326fe40868e333796bfeae17fb58ef709de8a7e8c73850ceca&"
+                                   "client_secret=5a1b187745e79a6cb2fe6b005a3b3be16b633411374eacb84a38c3c4068fbf50&"
+                                   "code=" + match.captured(1) + "&redirect_uri=http://localhost";
+
+                qWarning() << " Auth url: " << postData;
+                //connect(nm, SIGNAL(finished(QNetworkReply*)), this, SLOT(on_loadFinished(QNetworkReply*)));
+                view->load(*request, QNetworkAccessManager::PostOperation, postData.toUtf8());
+                //nm->post(*request, postData.toUtf8());
+            }
+                break;
+        }
+    }
 }
--- src/sparkan/webauth.h
+++ src/sparkan/webauth.h
@@ -2,6 +2,9 @@
 #define WEBAUTH_H
 #include <QObject>
 #include <QtWebKit/QWebView>
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
 
 class WebAuth : public QObject
 {
@@ -9,9 +12,10 @@ class WebAuth : public QObject
 public slots:
     void on_ulrChanged(QUrl url);
     void on_loadStarted(void);
-    void on_loadFinished(QNetworkReply* reply);
+    void on_loadFinished(bool ok);
     void on_loadProgress(int progress);
     void on_linkClicked(QUrl url);
+    void nm_Finished(QNetworkReply *reply);
 
 public:
     WebAuth();
@@ -24,6 +28,7 @@ private:
     QWebView *view;
     QWebPage *page;
     QWebFrame *frame;
+    QString code;
 
 };
 #endif // WEBAUTH_H
