ba20ab0c01df2480800c3384b8b0b5b0763e7343
milos <milos@localhost.localdomain>
2016-08-14 17:08:34 +0200

    Deleting unused files from Utils

--- Utils/BlockingQueue/Include/QueueOperationStatusEnum.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-enum class QueueOperationStatus {
-    SUCCESS , 
-    FULL    , 
-    EMPTY   , 
-    BUSY    , 
-    CLOSED
-};
--- Utils/Requests/Source/PostRequest.cpp
+++
@@ -1,7 +0,0 @@
-#include "PostRequest.h"
-
-namespace Http {
-    Method PostRequest::GetMethod() {
-        return Method::POST;
-    }    
-}
